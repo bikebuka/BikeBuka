4a36f4ebfb697653f1ef5c9be496e29794fb9461
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-01-10 19:46:26 +0300

    added home skeleton layout

--- app/src/main/java/com/bikebuka/bikebuka/ui/view/HomeActivity.kt
+++ app/src/main/java/com/bikebuka/bikebuka/ui/view/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.bikebuka.bikebuka.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.bikebuka.bikebuka.R
+import com.bikebuka.bikebuka.ui.viewmodel.HomeViewModel
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var viewModel: HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
     }
 }
