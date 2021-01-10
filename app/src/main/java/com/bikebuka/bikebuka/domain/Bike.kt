634f2dc69e036bc3fb45a87e5f90165cca3a2a57
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-01-10 14:36:45 +0300

    added primary key to Bike entity

--- app/src/main/java/com/bikebuka/bikebuka/domain/Bike.kt
+++ app/src/main/java/com/bikebuka/bikebuka/domain/Bike.kt
@@ -1,9 +1,12 @@
 package com.bikebuka.bikebuka.domain
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "bikes")
 data class Bike(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
     var name: String,
     var description: String,
     var price: String
