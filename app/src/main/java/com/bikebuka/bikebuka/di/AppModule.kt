878b33acfd60a6060a5d2d8f8633da5733efd32b
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-02-01 07:33:05 +0300

    added mpesa daraja library

--- app/src/main/java/com/bikebuka/bikebuka/di/AppModule.kt
+++ app/src/main/java/com/bikebuka/bikebuka/di/AppModule.kt
@@ -22,6 +22,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+
 @Module
 @InstallIn(ApplicationComponent::class)
 class AppModule {
